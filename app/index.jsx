d8221c3fba342d6ae77eba17b125d158502add5b
teinlb <tein.lebouille@gmail.com>
2024-09-12 16:33:02 +0200

    beta

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Tja</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl font-pblack">Tja</Text>
             <StatusBar style="auto" />
             <Link href="/profile" style={{ color: "blue" }}>
                 Go to Profile
@@ -13,13 +13,3 @@ export default function App() {
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
-// npx expo install expo-router react-native-safe-area-context react-native-screens expo-linking expo-constants expo-status-bar

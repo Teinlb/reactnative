2eebc48ca992ac5bb8d0f673929c364c92314724
Teinlb <tein.lebouille@gmail.com>
2024-09-17 22:45:22 +0200

    img test

--- app/profile.jsx
+++ app/profile.jsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 const Profile = () => {
     return (
         <View>
             <Text>Profile</Text>
+            <Image
+                source='./assets/images/profile.png'
+            />
         </View>
     );
 };
